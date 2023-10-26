a2807a5b0c5a660622eb0e93d498c4692ef393b1
Soumys56 <88768160+Soumys56@users.noreply.github.com>
2023-10-26 13:09:15 +0530

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -5,11 +5,18 @@ var mongoose = require('mongoose');
 // var mongoDB = `mongodb://127.0.0.1/${env. db}`;
 var mongoDB=process.env.MONGO_DB_URL;
 
-mongoose.connect(mongoDB, { useNewUrlParser: true });
- //Get the default connection
-var db = mongoose.connection;
-//Bind connection to error event (to get notification of connection errors)
-db.on('error', console.error.bind(console, 'MongoDB connection error:'));
-db.once('open',function(){
-    console.log("Db is connected")
-})
+// mongoose.connect(mongoDB, { useNewUrlParser: true });
+//  //Get the default connection
+// var db = mongoose.connection;
+// //Bind connection to error event (to get notification of connection errors)
+// db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+// db.once('open',function(){
+//     console.log("Db is connected")
+// })
+
+mongoose.connect(mongoDB).then(()=>{
+    
+    console.log("DB connected");
+    
+
+}).catch(error => console.log(error));
